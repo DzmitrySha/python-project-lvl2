018c584f9fb48236a20b03763cbdc016d8769bf3
DzmitrySha <ravenbydev@gmail.com>
2022-02-17 12:54:58 +0300

    modified gendiff.py added --format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,6 +10,7 @@ def main():
                                      description="Generate diff")
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args)
 
