97c562912b752f90f6d391dfc2fe82f74965675c
DzmitrySha <ravenbydev@gmail.com>
2022-02-20 22:08:28 +0300

    modified gendiff module, package, script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,17 +2,18 @@
 """Difference Generator."""
 
 import argparse
+from gendiff import generate_diff
 
 
 def main():
     """Get start here."""
     parser = argparse.ArgumentParser(prog='gendiff',
                                      description="Generate diff")
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
