f33a997477cf04d3e292bc77f3e395f5acdb062b
DzmitrySha <ravenbydev@gmail.com>
2022-03-27 14:04:04 +0300

    modified enter_parser.py

--- gendiff/enter_parser.py
+++ gendiff/enter_parser.py
@@ -4,10 +4,12 @@ import yaml
 
 
 def parser(file_path1, file_path2):
-    if os.path.splitext(file_path1) == '.json':
-        first_file = dict(json.load(open(file_path1)))
-        second_file = dict(json.load(open(file_path2)))
-    elif os.path.splitext(file_path1) == '.yaml' or '.yml':
-        first_file = dict(yaml.safe_load(open(file_path1)))
-        second_file = dict(yaml.safe_load(open(file_path2)))
-    return first_file, second_file
+    file1_ext = os.path.splitext(file_path1)
+    file2_ext = os.path.splitext(file_path2)
+    if (file1_ext and file2_ext) == '.json':
+        first_file = json.load(open(file_path1))
+        second_file = json.load(open(file_path2))
+    if (file1_ext and file2_ext) == '.yaml' or '.yml':
+        first_file = yaml.safe_load(open(file_path1))
+        second_file = yaml.safe_load(open(file_path2))
+    return dict(first_file), dict(second_file)
