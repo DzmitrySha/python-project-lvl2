8f7c3ee02ebdb35a799708e65ebf8bafc4b1a71e
DzmitrySha <ravenbydev@gmail.com>
2022-03-30 20:55:00 +0300

    modified test_gendiff.py and test_results.py

--- tests/fixtures/test_results.py
+++ tests/fixtures/test_results.py
@@ -1,4 +1,4 @@
-RESULT_JSON = """{
+RESULT_PLAIN = """{
   host: hexlet.io
 - timeout: 50
 + timeout: 20
@@ -7,12 +7,47 @@ RESULT_JSON = """{
 + verbose: true
 }"""
 
-
-RESULT_YML = """{
-  host: hexlet.io
-- timeout: 50
-+ timeout: 20
-- follow: false
-- proxy: 123.234.53.22
-+ verbose: true
+RESULT_TREE = """{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }"""
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,6 @@
 import pytest
 from gendiff import generate_diff
-from tests.fixtures.test_results import RESULT_JSON, RESULT_YML
+from tests.fixtures.test_results import RESULT_PLAIN
 
 
 @pytest.fixture
@@ -10,6 +10,10 @@ def paths():
 		"json2": "tests/fixtures/file2.json",
 		"yml1": "tests/fixtures/file1.yml",
 		"yml2": "tests/fixtures/file2.yml",
+		"tree_json1": "tests/fixtures/file_tree1.json",
+		"tree_json2": "tests/fixtures/file_tree2.json",
+		"tree_yml1": "tests/fixtures/file_tree1.yml",
+		"tree_yml2": "tests/fixtures/file_tree2.yml",
 	}
 	return paths
 
@@ -17,5 +21,5 @@ def paths():
 def test_generate_diff(paths):
 	result_diff_json = str(generate_diff(paths["json1"], paths["json2"]))
 	result_diff_yml = str(generate_diff(paths["yml1"], paths["yml2"]))
-	assert result_diff_json == RESULT_JSON
-	assert result_diff_yml == RESULT_YML
+	assert result_diff_json == RESULT_PLAIN
+	assert result_diff_yml == RESULT_PLAIN
