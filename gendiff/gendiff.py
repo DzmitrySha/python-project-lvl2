2d7d39477b3fdf251fd0f6a54dde62dc18de721f
DzmitrySha <ravenbydev@gmail.com>
2022-04-09 19:40:48 +0300

    modified gendiff.py (constants added)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,6 +4,11 @@ from gendiff.enter_parser import parser
 from gendiff.stylish import str_stylish
 
 
+ADDED = "+ "
+REMOVED = "- "
+UNCHANGED = "  "
+
+
 def make_diff(dict_1, dict_2):
     cross_dicts_keys = sorted(dict_1.keys() & dict_2.keys())
     dict1_unique_keys = sorted(dict_1.keys() - dict_2.keys())
@@ -16,18 +21,18 @@ def make_diff(dict_1, dict_2):
         child_2 = dict_2[key]
 
         if child_1 == child_2:
-            diff.append(f"  {key}: {child_1}")
+            diff.append(f"{UNCHANGED}{key}: {child_1}")
         elif isinstance(child_1, dict) and isinstance(child_2, dict):
             diff.append(key)
             diff.append(make_diff(child_1, child_2))
         else:
-            diff.append(f"- {key}: {child_1}")
-            diff.append(f"+ {key}: {child_2}")
+            diff.append(f"{REMOVED}{key}: {child_1}")
+            diff.append(f"{ADDED}{key}: {child_2}")
 
     for key in dict1_unique_keys:
-        diff.append(f"- {key}: {dict_1[key]}")
+        diff.append(f"{REMOVED}{key}: {dict_1[key]}")
     for key in dict2_unique_keys:
-        diff.append(f"+ {key}: {dict_2[key]}")
+        diff.append(f"{ADDED}{key}: {dict_2[key]}")
     return diff
 
 
