bfe82f15e2192949b6e579c974a52b63d0bef807
DzmitrySha <ravenbydev@gmail.com>
2022-04-09 18:45:28 +0300

    refactor enter_parser.py

--- gendiff/enter_parser.py
+++ gendiff/enter_parser.py
@@ -1,18 +1,21 @@
+# parsing module
+
 import os
 import json
 import yaml
 
 
 def parser(file_path1, file_path2):
+    """Make dicts from paths."""
     file1_ext = os.path.splitext(file_path1)
     file2_ext = os.path.splitext(file_path2)
 
     if (file1_ext and file2_ext) == '.json':
-        first_dict = json.load(open(file_path1))
-        second_dict = json.load(open(file_path2))
+        dict_1 = json.load(open(file_path1))
+        dict_2 = json.load(open(file_path2))
 
     if (file1_ext and file2_ext) == '.yaml' or '.yml':
-        first_dict = yaml.safe_load(open(file_path1))
-        second_dict = yaml.safe_load(open(file_path2))
+        dict_1 = yaml.safe_load(open(file_path1))
+        dict_2 = yaml.safe_load(open(file_path2))
 
-    return dict(first_dict), dict(second_dict)
+    return dict(dict_1), dict(dict_2)
