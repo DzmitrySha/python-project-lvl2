2d4a1041cf1b54ca60296f765e256e5c0e6676ac
DzmitrySha <ravenbydev@gmail.com>
2022-04-22 09:48:57 +0300

    modified stylish.py

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -57,24 +57,3 @@ def to_string(tree, depth=1):
 
 def stylish(diff: dict):
     return "{\n" + to_string(diff)
-
-
-# def to_str(diff):
-#     result = "{\n" + "\n".join(diff).lower() + "\n}"
-#     return result
-#
-#
-# def to_str_dict(diff: dict):
-#     result = [f"{key}: {value}" for key, value in diff.items()]
-#     return "".join(result).lower()
-#
-#
-# def stylish_plain(diff: dict):
-#     result = []
-#     for key, value in diff.items():
-#         if value["status"] == CHANGED:
-#             result.append(REMOVED + to_str_dict(value["diff_rem"]))
-#             result.append(ADDED + to_str_dict(value["diff_add"]))
-#         else:
-#             result.append(value["status"] + to_str_dict(value["diff"]))
-#     return to_str(result)
