ed98c8aab927e5178ef28406527defe517b034c1
DzmitrySha <ravenbydev@gmail.com>
2022-05-14 10:12:38 +0300

    modified tests: json, plain, stylish

--- tests/test_formatters/test_plain.py
+++ tests/test_formatters/test_plain.py
@@ -1,22 +1,20 @@
-# # test plain formatter (for tree structure)
-#
-# import pytest
-# from gendiff import generate_diff
-#
-# FORMATTER = 'plain'
-#
-#
-# @pytest.mark.asyncio
-# async def test_json_plain(file_tree1_json_path,
-#                           file_tree2_json_path,
-#                           result_render):
-#     assert result_render == generate_diff(file_tree1_json_path,
-#                                           file_tree2_json_path)
-#
-#
-# @pytest.mark.asyncio
-# async def test_yml_plain(file_tree1_yml_path,
-#                          file_tree2_yml_path,
-#                          result_render):
-#     assert result_render == generate_diff(file_tree1_yml_path,
-#                                           file_tree2_yml_path)
+# test plain formatter (for tree structure)
+
+import pytest
+from gendiff import generate_diff
+
+FORMATTER = 'plain'
+
+
+@pytest.mark.asyncio
+async def test_json(file_tree1_json_path, file_tree2_json_path, result_render):
+    assert result_render == generate_diff(file_tree1_json_path,
+                                          file_tree2_json_path,
+                                          format_name=FORMATTER)
+
+
+@pytest.mark.asyncio
+async def test_yml(file_tree1_yml_path, file_tree2_yml_path, result_render):
+    assert result_render == generate_diff(file_tree1_yml_path,
+                                          file_tree2_yml_path,
+                                          format_name=FORMATTER)
--- tests/test_formatters/test_stylish.py
+++ tests/test_formatters/test_stylish.py
@@ -7,16 +7,12 @@ FORMATTER = 'stylish'
 
 
 @pytest.mark.asyncio
-async def test_json_tree(file_tree1_json_path,
-                         file_tree2_json_path,
-                         result_render):
+async def test_json(file_tree1_json_path, file_tree2_json_path, result_render):
     assert result_render == generate_diff(file_tree1_json_path,
                                           file_tree2_json_path)
 
 
 @pytest.mark.asyncio
-async def test_yml_tree(file_tree1_yml_path,
-                        file_tree2_yml_path,
-                        result_render):
+async def test_yml(file_tree1_yml_path, file_tree2_yml_path, result_render):
     assert result_render == generate_diff(file_tree1_yml_path,
                                           file_tree2_yml_path)
