87c0a3578e625b5ea0701abe21f90f12c6e453b9
DzmitrySha <ravenbydev@gmail.com>
2022-05-25 14:39:34 +0300

    modified gendiff.py (build_tree)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,12 +1,12 @@
 # generate diff module
 
 from gendiff.cli import parser
-from gendiff.tree import make_tree
+from gendiff.tree import build_tree
 from gendiff.formatters import format_
 
 
 def generate_diff(file_path1: str, file_path2: str, format_name="stylish"):
     dict_1, dict_2 = parser(file_path1, file_path2)
-    tree = make_tree(dict_1, dict_2)
+    tree = build_tree(dict_1, dict_2)
     diff = format_(tree, format_name)
     return diff
