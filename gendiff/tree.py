2ab00b284a3bd2b9db1dcc1fb630c37d717db2a0
Dzmitry <73607453+DzmitrySha@users.noreply.github.com>
2022-05-26 11:36:48 +0300

    refactor variable names, remove unuseful func

--- gendiff/tree.py
+++ gendiff/tree.py
@@ -1,9 +1,9 @@
 # build internal tree module
 
 def make_tree(dict_1, dict_2) -> list:
-    cross_dicts_keys = sorted(dict_1.keys() & dict_2.keys())
-    dict1_unique_keys = sorted(dict_1.keys() - dict_2.keys())
-    dict2_unique_keys = sorted(dict_2.keys() - dict_1.keys())
+    cross_dicts_keys = dict_1.keys() & dict_2.keys()
+    dict1_unique_keys = dict_1.keys() - dict_2.keys()
+    dict2_unique_keys = dict_2.keys() - dict_1.keys()
 
     result = []
     for key in cross_dicts_keys:
@@ -14,36 +14,36 @@ def make_tree(dict_1, dict_2) -> list:
             result.append({
                 'key': key,
                 'type': 'unchanged',
-                'value': dict_1[key],
+                'value': child_1,
             })
         elif isinstance(child_1, dict) and isinstance(child_2, dict):
             result.append({
                 'key': key,
                 'type': 'nested',
-                'children': make_tree(dict_1[key], dict_2[key]),
+                'children': make_tree(child_1, child_2),
             })
         else:
             result.append({
                 'key': key,
                 'type': 'changed',
-                'old_value': dict_1[key],
-                'new_value': dict_2[key],
+                'old_value': child_1,
+                'new_value': child_2,
             })
 
     for key in dict1_unique_keys:
         result.append({
             'key': key,
             'type': 'removed',
-            'value': dict_1[key]
+            'value': child_1
         })
 
     for key in dict2_unique_keys:
         result.append({
             'key': key,
             'type': 'added',
-            'value': dict_2[key]
+            'value': child_2
         })
-    result = sorted(result, key=lambda child: child['key'])
+    result = sorted(result, key=lambda node: node['key'])
     return result
 
 
