e670228ff844f33658efe11484aa0da314655e53
DzmitrySha <ravenbydev@gmail.com>
2022-05-28 18:41:26 +0300

    refactor (make_tree function)

--- gendiff/tree.py
+++ gendiff/tree.py
@@ -1,49 +1,45 @@
 # build internal tree module
 
 def make_tree(dict_1, dict_2) -> list:
-    cross_dicts_keys = dict_1.keys() & dict_2.keys()
-    dict1_unique_keys = dict_1.keys() - dict_2.keys()
-    dict2_unique_keys = dict_2.keys() - dict_1.keys()
+    keys = dict_1.keys() | dict_2.keys()
 
     result = []
-    for key in cross_dicts_keys:
+    for key in sorted(keys):
         child_1 = dict_1.get(key)
         child_2 = dict_2.get(key)
 
-        if child_1 == child_2:
+        if key not in dict_1:
             result.append({
                 'key': key,
-                'type': 'unchanged',
-                'value': dict_1[key],
+                'type': 'added',
+                'value': child_2
+            })
+        elif key not in dict_2:
+            result.append({
+                'key': key,
+                'type': 'removed',
+                'value': child_1
             })
         elif isinstance(child_1, dict) and isinstance(child_2, dict):
             result.append({
                 'key': key,
                 'type': 'nested',
-                'children': make_tree(dict_1[key], dict_2[key]),
+                'children': make_tree(child_1, child_2),
+            })
+        elif child_1 == child_2:
+            result.append({
+                'key': key,
+                'type': 'unchanged',
+                'value': child_1,
             })
         else:
             result.append({
                 'key': key,
                 'type': 'changed',
-                'old_value': dict_1[key],
-                'new_value': dict_2[key],
+                'old_value': child_1,
+                'new_value': child_2,
             })
 
-    for key in dict1_unique_keys:
-        result.append({
-            'key': key,
-            'type': 'removed',
-            'value': dict_1[key]
-        })
-
-    for key in dict2_unique_keys:
-        result.append({
-            'key': key,
-            'type': 'added',
-            'value': dict_2[key]
-        })
-    result = sorted(result, key=lambda node: node['key'])
     return result
 
 
