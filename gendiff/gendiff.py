f08326d8c3c29c1be92b994567a192a6685a9391
DzmitrySha <ravenbydev@gmail.com>
2022-05-29 12:28:07 +0300

    refactor (added args types)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,7 +5,9 @@ from gendiff.tree import build_tree
 from gendiff.formatters import format_
 
 
-def generate_diff(file_path1: str, file_path2: str, format_name="stylish"):
+def generate_diff(file_path1: str,
+                  file_path2: str,
+                  format_name="stylish") -> str:
     dict_1, dict_2 = parser(file_path1, file_path2)
     tree = build_tree(dict_1, dict_2)
     diff = format_(tree, format_name)
