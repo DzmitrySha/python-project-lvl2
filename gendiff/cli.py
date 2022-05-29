d18220bd6b929a691ed51e021f8ec1c16af45630
DzmitrySha <ravenbydev@gmail.com>
2022-05-29 12:27:31 +0300

    refactor (added args types, raise Error)

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,7 +5,7 @@ import json
 import yaml
 
 
-def parser(file_path1, file_path2):
+def parser(file_path1: str, file_path2: str) -> tuple:
     """Make dicts from paths."""
     file1_ext = os.path.splitext(file_path1)
     file2_ext = os.path.splitext(file_path2)
@@ -14,8 +14,12 @@ def parser(file_path1, file_path2):
         dict_1 = json.load(open(file_path1))
         dict_2 = json.load(open(file_path2))
 
-    if (file1_ext and file2_ext) == '.yaml' or '.yml':
+    elif (file1_ext and file2_ext) == '.yaml' or '.yml':
         dict_1 = yaml.safe_load(open(file_path1))
         dict_2 = yaml.safe_load(open(file_path2))
+
+    else:
+        raise TypeError(f"Invalid file extension. "
+                        f"Must be: .JSON, .YAML or .YML")
 
     return dict(dict_1), dict(dict_2)
