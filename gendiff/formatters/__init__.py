02436d69de75c9471fc2292329cb00026f74dd7f
DzmitrySha <ravenbydev@gmail.com>
2022-06-07 21:48:37 +0300

    replace formatter.py to gendiff folder

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,4 +1,4 @@
 from gendiff.formatters.stylish import format_stylish  # noqa: F401
 from gendiff.formatters.plain import format_plain  # noqa: F401
 from gendiff.formatters.json import format_json  # noqa: F401
-from gendiff.formatters.formatter import format_  # noqa: F401
+from gendiff.formatter import format_  # noqa: F401
