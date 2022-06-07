65c91dc97b1c133538e6dba2a8471264207c4fe7
DzmitrySha <ravenbydev@gmail.com>
2022-06-07 23:39:33 +0300

    refactor parser.py and gendiff.py functions

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,14 +1,29 @@
 # generate diff module
 
-from gendiff.parser import parser
+import os
+from gendiff.parser import parse
 from gendiff.tree import build_tree
 from gendiff.formatters import format_
+
+
+def get_file_extension(file_path: str) -> str:
+    """Get file extension from file path."""
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension
+
+
+def get_file_data(file_path: str) -> str:
+    """Get data from file."""
+    file_extension = get_file_extension(file_path)
+    file_data = parse(file_path, file_extension)
+    return file_data
 
 
 def generate_diff(file_path1: str,
                   file_path2: str,
                   format_name="stylish") -> str:
-    dict_1, dict_2 = parser(file_path1, file_path2)
+    dict_1 = dict(get_file_data(file_path1))
+    dict_2 = dict(get_file_data(file_path2))
     tree = build_tree(dict_1, dict_2)
     diff = format_(tree, format_name)
     return diff
