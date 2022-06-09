4489bb9c815ad9125d7a580c0440998ba4827171
DzmitrySha <ravenbydev@gmail.com>
2022-06-09 23:35:13 +0300

    modified plain.py logic (if - return)

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,15 +1,15 @@
 # make format plain module
 
-def make_phrase(phrase_type: str, path="", old_value="", new_value="") -> str:
-    phrases = {
-        "removed": f"Property {path} was removed",
-        "added": f"Property {path} was added with value: {new_value}",
-        "updated": f"Property {path} was updated."
-                   f" From {old_value} to {new_value}",
-    }
-    if phrase_type in phrases:
-        return phrases[phrase_type]
-    raise ValueError(f'Unknown phrase: {phrase_type}')
+# def make_phrase(phrase_type: str, path="", old_value="", new_value="") -> str:
+#     phrases = {
+#         "removed": f"Property {path} was removed",
+#         "added": f"Property {path} was added with value: {new_value}",
+#         "updated": f"Property {path} was updated."
+#                    f" From {old_value} to {new_value}",
+#     }
+#     if phrase_type in phrases:
+#         return phrases[phrase_type]
+#     raise ValueError(f'Unknown phrase: {phrase_type}')
 
 
 def to_string(value):
@@ -24,41 +24,39 @@ def to_string(value):
     return f"'{value}'"
 
 
-def iter_(node: dict, path='') -> str:
+def iter_(node: dict, path="") -> str:
+    if node['type'] == 'unchanged':
+        return ''
+
     children = node.get('children')
-    result = []
+    current_path = f"{path}{node.get('key')}"
 
-    for child in children:
-        current_path = f"{path}{child['key']}"
+    if node['type'] == 'root':
+        lines = map(lambda child: iter_(child, path), children)
+        result = "\n".join(lines)
+        return result.replace('\n\n', '\n')
 
-        if child['type'] == 'nested':
-            result.append(iter_(child, f"{current_path}."))
+    if node['type'] == 'nested':
+        lines = map(lambda child: iter_(child, f"{current_path}."), children)
+        result = "\n".join(lines)
+        return result
 
-        elif child['type'] == 'removed':
-            phrase = make_phrase("removed", to_string(current_path))
-            result.append(phrase)
+    if node['type'] == 'removed':
+        return f"Property '{current_path}' was removed"
 
-        elif child['type'] == 'added':
-            added_value = to_string(child["value"])
-            phrase = make_phrase("added", to_string(current_path),
-                                 new_value=added_value)
-            result.append(phrase)
+    if node['type'] == 'added':
+        formatted_value = to_string(node.get('value'))
+        return f"Property '{current_path}' was added " \
+               f"with value: {formatted_value}"
 
-        elif child['type'] == 'changed':
-            old_value = to_string(child["old_value"])
-            new_value = to_string(child["new_value"])
-            phrase = make_phrase("updated", to_string(current_path),
-                                 old_value=old_value, new_value=new_value)
-            result.append(phrase)
+    if node['type'] == 'changed':
+        formatted_old_value = to_string(node.get('old_value'))
+        formatted_new_value = to_string(node.get('new_value'))
+        return f"Property '{current_path}' was updated. " \
+               f"From {formatted_old_value} to {formatted_new_value}"
 
-        elif child['type'] == 'unchanged':
-            continue
-
-        else:
-            raise ValueError(f"Unknown child type: {child['type']}")
-
-    return "\n".join(result)
+    raise TypeError('Unknown node type')
 
 
-def format_plain(node):
+def format_plain(node: dict):
     return iter_(node)
