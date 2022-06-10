caba75340a1d80dda641a874c618445874aa0d61
DzmitrySha <ravenbydev@gmail.com>
2022-06-10 12:31:44 +0300

    modified stylish.py (indents)

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,15 +1,7 @@
 # make stylish module
 
-def get_indent(depth: int, node_type='root') -> str:
-    indent = " "
-    marked_nodes = ['changed', 'removed', 'added']
-    unmarked_nodes = ['root', 'nested', 'unchanged']
-    if node_type in marked_nodes:
-        return indent * (depth * 4 - 2)
-    elif node_type in unmarked_nodes:
-        return indent * depth * 4
-    else:
-        raise ValueError(f'Unknown node: {node_type}')
+def get_indent(depth: int) -> str:
+    return " " * (depth * 4 - 2)
 
 
 def to_string(value, depth: int) -> str:
@@ -21,15 +13,15 @@ def to_string(value, depth: int) -> str:
     if isinstance(value, dict):
         lines = []
         for k, v in value.items():
-            lines.append(f"{indent}    {k}: {to_string(v, depth + 1)}")
+            lines.append(f"{indent}      {k}: {to_string(v, depth + 1)}")
         result = "\n".join(lines)
-        return f'{{\n{result}\n{indent}}}'
+        return f'{{\n{result}\n  {indent}}}'
     return value
 
 
 def iter_(node: dict, depth=0) -> str:
     children = node.get('children')
-    indent = get_indent(depth, node['type'])
+    indent = get_indent(depth)
     formatted_value = to_string(node.get('value'), depth)
     formatted_value1 = to_string(node.get('old_value'), depth)
     formatted_value2 = to_string(node.get('new_value'), depth)
@@ -42,7 +34,7 @@ def iter_(node: dict, depth=0) -> str:
     if node['type'] == 'nested':
         lines = map(lambda child: iter_(child, depth + 1), children)
         result = '\n'.join(lines)
-        return f"{indent}{node['key']}: {{\n{result}\n{indent}}}"
+        return f"{indent}  {node['key']}: {{\n{result}\n  {indent}}}"
 
     if node['type'] == 'changed':
         line1 = f"{indent}- {node['key']}: {formatted_value1}\n"
@@ -51,7 +43,7 @@ def iter_(node: dict, depth=0) -> str:
         return result
 
     if node['type'] == 'unchanged':
-        return f"{indent}{node['key']}: {formatted_value}"
+        return f"{indent}  {node['key']}: {formatted_value}"
 
     if node['type'] == 'removed':
         return f"{indent}- {node['key']}: {formatted_value}"
