6d5a9401d2728753d1520dfc592290caeff358b4
DzmitrySha <ravenbydev@gmail.com>
2022-06-16 08:46:27 +0300

    modified parser.py (if-else)

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -7,7 +7,7 @@ import yaml
 def parse(data, format_name):
     if format_name == '.json':
         return json.load(open(data))
-    elif format_name in ('.yml', '.yaml'):
+    if format_name in ('.yml', '.yaml'):
         return yaml.safe_load(open(data))
-    else:
-        raise OSError('Unknown file format')
+
+    raise ValueError('Unknown file format')
